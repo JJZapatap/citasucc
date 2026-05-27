6ceb045cb6ddb4fb0af4e3c4e27d072f4ab5164d
JJZapatap <156350000+JJZapatap@users.noreply.github.com>
2026-05-27 07:21:34 -0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+// Pegue aquí la URL /exec de la implementación de Google Apps Script.
+// Esta misma configuración la usan index.html y citas.html.
+window.APP_CONFIG = {
+  API_URL: "https://script.google.com/macros/s/AKfycbyyniFE2G5A7OcVyRQswtHzNj9W3hux2mxFr4MpRR8DUA37x7xiSkW7xq0UWXVf0fUh/exec"
+};
