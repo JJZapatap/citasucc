b47917f11a962102ecbfb02f55441f421ae85e05
JJZapatap <156350000+JJZapatap@users.noreply.github.com>
2026-05-28 15:54:44 -0500

    Delete config_.js

--- config_.js
+++
@@ -1,5 +0,0 @@
-// Pegue aquí la URL /exec de la implementación de Google Apps Script.
-// Esta misma configuración la usan index.html y citas.html.
-window.APP_CONFIG = {
-  API_URL: "https://script.google.com/macros/s/AKfycbxyR2rAetH4Dou2GWrKxDBf7MetP5Tu8bpzefE6rVgSOzbpv5X7GyO4DMHKL1SFDQik/exec"
-};
